90dd08e91ee44334aac93cf6eba87a8a8cbae5f8
Nikita <mitichkin0303@mail.ru>
2023-09-16 22:08:03 +0200

    1

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Upi");
+}
+else
+{
+Console.Write("Hello, ");
+Console.WriteLine(username);
+}
