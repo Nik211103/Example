96b0b344b426af934fc552fc1fcc86c50ccfa656
Nikita <mitichkin0303@mail.ru>
2023-09-21 01:46:12 +0200

    Нарисовал картинку

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,12 +1,16 @@
-﻿Console.WriteLine("Write your name");
-string username = Console.ReadLine();
+﻿class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите первое число:");
+        int a = int.Parse(Console.ReadLine());
 
-if(username.ToLower() == "masha")
-{
-    Console.WriteLine("Upi");
-}
-else
-{
-Console.Write("Hello, ");
-Console.WriteLine(username);
-}
+        Console.WriteLine("Введите второе число:");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > b) {
+            Console.WriteLine($"{a} больше {b}");
+        } else if (a < b) {
+            Console.WriteLine($"{b} больше {a}");
+        } else {
+            Console.WriteLine("Числа равны");
+        }
+    }
