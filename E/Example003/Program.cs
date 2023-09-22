676a55fa1a3e7dfa4efed128c91e5b98df0e25e4
Nikita <mitichkin0303@mail.ru>
2023-09-22 23:35:54 +0200

    Создал свой калькулятор с простыми и сложными действиями

--- E/Example003/Program.cs
+++ E/Example003/Program.cs
@@ -1,4 +1,74 @@
-﻿int numberA = 3;
-int numberB = 5;
-int result = numberA + numberB;
+﻿
+using System;
+using System.Text.RegularExpressions;
+System.Console.WriteLine("Выберите действие:");
+
+System.Console.WriteLine("1.Простые действия");
+System.Console.WriteLine("2.Сложные действия");
+    double result = 0;
+    int arg1 = int.Parse(Console.ReadLine());
+    if (arg1 == 1) {
+        System.Console.WriteLine("1.Умножение");
+        System.Console.WriteLine("2.Сложение");
+        System.Console.WriteLine("3.Деление");
+        System.Console.WriteLine("4.Вычитание");
+
+            int arg2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите первое число:");
+            double a = double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Введите второе число:");
+            double b = double.Parse(Console.ReadLine());
+
+            if (arg2 == 1) {
+                result = a * b;
+            }
+            
+            if (arg2 == 2) {
+                result = a + b;
+            }
+
+            if (arg2 == 3) {
+                    if (b == 0){
+                    System.Console.WriteLine("Ошибка: Деление на ноль");
+                    return;
+                    }
+                result = a / b;
+            }
+
+            if (arg2 == 4) {
+                result = a - b;
+            }
+    }
+    if (arg1 == 2) {
+    System.Console.WriteLine("1.Возведение в квадрат");
+    System.Console.WriteLine("2.Квадратный корень числа");
+        int arg2 = int.Parse(Console.ReadLine());
+
+            if (arg2 == 1){
+            System.Console.WriteLine("Введите число");
+            double a = double.Parse(Console.ReadLine());
+            result = a * a;
+            }
+
+            if (arg2 == 2){
+            System.Console.WriteLine("Введите число");
+            double a = double.Parse(Console.ReadLine());
+            double squareRoot = Math.Sqrt(a);
+            result = squareRoot;
+            }
+           
+
+}
+            
+
+System.Console.WriteLine("Результат:");
 System.Console.WriteLine(result);
+
+    
+
+
+
+
+
