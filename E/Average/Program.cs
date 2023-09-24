9ab15fbb14fea4c2392685fdcc38474ddd33b7f8
Nikita <mitichkin0303@mail.ru>
2023-09-24 02:01:37 +0200

    Создал гениратор массива и нахождение его среднего арифметического

---
+++ E/Average/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+/*Напишите функцию, которая принимает массив рандомных чисел в заданом диапозоне и возвращает среднее значение этих чисел.*/
+System.Console.WriteLine("Введите начало диапазона:");
+int arg1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите конец диапазона:");
+int arg2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите желаемую длину массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size]; 
+
+Random random = new Random();
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = random.Next(arg1, arg2 + 1);
+}
+
+System.Console.WriteLine($"Сгенерированный набор чисел: {string.Join(", ", array)}");
+
+double average = array.Average();
+System.Console.WriteLine($"Среднее арифметическое данного цикла: {average}");
+
--- E/Example010/Program.cs
+++ E/Example010/Program.cs
@@ -69,10 +69,10 @@ int a = int.Parse(Console.ReadLine());
     }*/
 
 System.Console.WriteLine("Введите первое число:");
-int a = int.Parse(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
 System.Console.WriteLine("Введите второе число:");
-int b = int.Parse(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
     if (a / b == b){
     System.Console.WriteLine($"{b} является квадратом {a}.");
     }
