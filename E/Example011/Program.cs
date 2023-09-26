744d90c9832eb6b5971cc518a82426bdc22d5a65
Nikita <mitichkin0303@mail.ru>
2023-09-26 23:56:39 +0200

    Решил все задачи по 2 семинару, у меня они работают и вообщ лучше чем на дурацкой платформе

--- E/Example011/Program.cs
+++ E/Example011/Program.cs
@@ -34,14 +34,64 @@ switch (a)
 
 
 
-
-
-
-System.Console.WriteLine("Введите число n:");
+/*Console.WriteLine("Введите число n:");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
 {
     double powI = Math.Pow(i, 3);
-    System.Console.Write($"{powI}, ");
+    Console.Write($"{powI}, ");
 }
-System.Console.Write("\b\b ");
+Console.Write("\b\b ");*/
+
+
+
+
+
+/*System.Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+ 
+    if (9999 < number && number < 99999)
+    {
+    int first = number / 10000; //5 000
+    int second = number / 1000%10;// 5 0 00
+    int third = number / 100%10;
+    int fourth = number /10%10;//500 0
+    int fifth = number%10;
+
+        if (first == fifth && second == fourth)
+        {
+        System.Console.WriteLine($"Число {number} палиндром");
+        }
+        else
+        {
+        System.Console.WriteLine($"Число {number} не палиндром");
+        }
+    }
+    else
+    {
+    System.Console.WriteLine("Введенно не пятизначное число.");
+    }*/
+
+
+
+
+/*Console.WriteLine("Введите координату X1:");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y1:");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z1:");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X2:");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y2:");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z2:");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+double arg1 = Math.Sqrt (Math.Pow((X1 - X2), 2) + Math.Pow((Y1 -Y2), 2) + Math.Pow((Z1 - Z2), 2));
+double result = Math.Round(arg1, 2);
+Console.WriteLine(result);*/
