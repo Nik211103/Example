415ec9ff9e6f3dc035bc56002c684eb360572ce7
Nikita <mitichkin0303@mail.ru>
2023-09-30 00:08:50 +0200

    НЕ доделал семинар

---
+++ E/Example014/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+/*System.Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+System.Console.WriteLine($"Сумма числе от 1 до {A} равна {Sum(A)}");*/
+
+void Mass(int col)
+{
+    int[] array = new int[col];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write($"{array[i]} ");
+    }
+
+    System.Console.WriteLine("_______________________");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        System.Console.Write($"{array[i]} ");
+    }
+    
+}
+System.Console.Write("Введите длинну массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Mass(n);
