e488343dc169e2d5710edadcb8c3e3be47d47ad3
Nikita <mitichkin0303@mail.ru>
2023-09-30 22:26:02 +0200

    Прорешал семинар и решил дз

--- E/Example014/Program.cs
+++ E/Example014/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-/*System.Console.Write("Введите число A: ");
+﻿/*System.Console.Write("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
 
 int Sum(int limit)
@@ -36,3 +34,117 @@ System.Console.WriteLine($"Сумма числе от 1 до {A} равна {Sum
 System.Console.Write("Введите длинну массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Mass(n);*/
+
+
+
+/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+/*int NumCount(int a)
+{
+    int count = 0;
+    while (a > 0)
+    {
+        a/=10;
+        count++;
+    }
+    return count;
+}
+System.Console.WriteLine($"Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Количество цифр в числе = {NumCount(a)}");*/
+
+/*Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+/*int Factorial(int n)
+{
+int fact = 1 ;
+for (int i = 1; i <= n; i++)
+{
+fact*= i;
+}
+return fact;
+}
+Console.Write($"Введите число: ");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Факториал числа {a} = {Factorial(a)}");*/
+
+/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+ /*int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        System.Console.Write($"{array[i]} ");
+    }*/
+
+
+/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+/*System.Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите числе B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int result = 1;
+if (B > 0)
+{
+    while (count <= B )
+    {
+        result *= A;
+        count++;   
+    }
+System.Console.WriteLine($"Число {A} в степени {B} = {result}.");
+}
+else
+{
+    System.Console.WriteLine($"Число B должно быть больше нуля. Число которое вы ввели {B}");
+}*/
+
+
+/*Задача 27.Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+/*int NumCount(int a)
+{
+   
+    int sum = 0;
+    int b;
+    while (a > 0)
+    {
+        b = a % 10;
+        sum += b;
+        a /= 10;
+    }
+    return sum;
+}
+System.Console.WriteLine($"Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма цифр в числе = {NumCount(a)}");*/
+    
+/*Задача 29.Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+
+
+/*int Lenght = 8;
+int[] array = new int [Lenght];
+System.Console.WriteLine("Введите число min ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число max ");
+int max = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+        System.Console.Write($"{array[i]} ");
+    }*/
+
+
+
