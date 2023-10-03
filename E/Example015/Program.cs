8728485fd724df33007fd76696a830bb8d707c46
Nikita <mitichkin0303@mail.ru>
2023-10-03 23:30:02 +0200

    Начал решать задачи из 4 семинара

---
+++ E/Example015/Program.cs
@@ -0,0 +1,34 @@
+﻿/*int[] GerAraay(int size, int min, int max)
+{
+    int[] res = new int[size];
+    for( int i =0; i < size; i++)
+    {
+        res[i] = new Random().Next(min, max + 1);
+        System.Console.Write($"{res[i]}, ");
+    }
+    System.Console.WriteLine("\b\b ");
+    System.Console.WriteLine();
+    return res;
+}
+Console.WriteLine("Введите число элементов массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите максимальный элемент массива ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите минимальный элемент массива ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = GerAraay(n, min, max);
+
+int sumPos = 0;
+int sumNeg = 0;
+
+ for( int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sumPos += arr[i];
+        else sumNeg += arr[i];
+    }
+System.Console.WriteLine($"Сумма положительных = {sumPos}");
+System.Console.WriteLine($"Сумма отрицательных = {sumNeg}");*/
+
